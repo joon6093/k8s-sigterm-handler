c4cca038cb75b840169927715651b94765a679e9
jeyong <118044367+joon6093@users.noreply.github.com>
2024-11-28 13:48:45 +0900

    feat: 원할한 사용을 위한 javadoc 작성

--- handler/src/main/java/io/jeyong/handler/SigtermHandlerProperties.java
+++ handler/src/main/java/io/jeyong/handler/SigtermHandlerProperties.java
@@ -1,7 +1,48 @@
 package io.jeyong.handler;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
+// @formatter:off
+import org.springframework.boot.context.properties.ConfigurationProperties; /**
+ * Configuration properties for Kubernetes SIGTERM handling.
+ *
+ * <p>
+ * Allows customization of the SIGTERM handler's behavior, including whether it is enabled and the exit code to use
+ * during graceful termination.
+ * </p>
+ *
+ * <ul>
+ *     <li><b>kubernetes.handler.enabled:</b> Set whether the handler is enabled or disabled (default: true).</li>
+ *     <li><b>kubernetes.handler.exit-code:</b> Sets the exit code for graceful application termination (default: 0).</li>
+ * </ul>
+ *
+ * <pre>
+ * Example configuration (YAML):
+ * {@code
+ * kubernetes:
+ *   handler:
+ *     enabled: true
+ *     exit-code: 1
+ * }
+ * </pre>
+ *
+ * <pre>
+ * Example configuration (Properties):
+ * {@code
+ * kubernetes.handler.enabled=true
+ * kubernetes.handler.exit-code=1
+ * }
+ * </pre>
+ *
+ * <p>
+ * By default, the handler is enabled, and the application terminates with an exit code of 0,
+ * marking the Kubernetes Pod as "Completed."
+ * </p>
+ *
+ * @author jeyong
+ * @since 1.0
+ * @see SigtermHandlerConfiguration
+ */
+// @formatter:on
+@ConfigurationProperties(prefix = "kubernetes.handler")
 public class SigtermHandlerProperties {
 
     private boolean enabled = true;
