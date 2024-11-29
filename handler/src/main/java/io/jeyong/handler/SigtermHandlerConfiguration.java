6efe566d70e3b6245d40d573eb390d5fb80f6448
jeyong <joon6093@naver.com>
2024-11-30 07:35:36 +0900

    feat: 라이브러리 사용 시 초기화 메세지를 통해 설정 정보를 출력하도록 구현

--- handler/src/main/java/io/jeyong/handler/SigtermHandlerConfiguration.java
+++ handler/src/main/java/io/jeyong/handler/SigtermHandlerConfiguration.java
@@ -1,5 +1,9 @@
 package io.jeyong.handler;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -16,12 +20,26 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(SigtermHandlerProperties.class)
 public class SigtermHandlerConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(SigtermHandlerConfiguration.class);
     private static final String SIGNAL_TYPE = "TERM";
 
     private final SigtermHandlerProperties sigtermHandlerProperties;
 
     public SigtermHandlerConfiguration(final SigtermHandlerProperties sigtermHandlerProperties) {
         this.sigtermHandlerProperties = sigtermHandlerProperties;
+    }
+
+    @PostConstruct
+    public void logInitialization() {
+        if (Strings.isBlank(sigtermHandlerProperties.getTerminationMessagePath())) {
+            logger.info("Sigterm handler initialized with exitCode: {}", sigtermHandlerProperties.getExitCode());
+        } else {
+            logger.info(
+                    "Sigterm handler initialized with exitCode: {}, terminationMessagePath: {}, terminationMessage: '{}'",
+                    sigtermHandlerProperties.getExitCode(),
+                    sigtermHandlerProperties.getTerminationMessagePath(),
+                    sigtermHandlerProperties.getTerminationMessage());
+        }
     }
 
     @Bean
