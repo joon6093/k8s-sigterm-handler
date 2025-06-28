0264a9236e3ef4c9949131a4f333443e8dc9b219
jeyong <joon6093@naver.com>
2025-06-28 20:19:51 +0900

    refactor: 불필요한 Bean을 등록하지 않도록 변경

--- handler/src/main/java/io/jeyong/handler/SigtermHandlerConfiguration.java
+++ handler/src/main/java/io/jeyong/handler/SigtermHandlerConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @AutoConfiguration
 @ConditionalOnProperty(
@@ -44,13 +43,13 @@ public class SigtermHandlerConfiguration {
     }
 
     @Bean
-    public ApplicationTerminator applicationTerminator(final ApplicationContext applicationContext) {
-        return new SpringContextTerminator(applicationContext, sigtermHandlerProperties.getExitCode(),
-                sigtermHandlerProperties.getTerminationMessagePath(), sigtermHandlerProperties.getTerminationMessage());
-    }
+    public SignalHandlerRegistrar sigtermHandlerRegister(final ApplicationContext applicationContext) {
+        final ApplicationTerminator applicationTerminator = new SpringContextTerminator(
+                applicationContext,
+                sigtermHandlerProperties.getExitCode(),
+                sigtermHandlerProperties.getTerminationMessagePath(),
+                sigtermHandlerProperties.getTerminationMessage());
 
-    @Bean
-    public SignalHandlerRegistrar sigtermHandlerRegister(final ApplicationTerminator applicationTerminator) {
         return new SignalHandlerRegistrar(applicationTerminator, SIGNAL_TYPE);
     }
 }
